cb98f86647ba141cd0d1e2131255b2625049efa4
Alvaro Martinez <ajmartinez8789@gmail.com>
2024-05-21 23:44:11 -0500

    se trabajo en el fechainico  de tiempo completo

--- app/src/main/java/com/example/nominavalen/ui/tcompleto/TcompletoFragment.java
+++ app/src/main/java/com/example/nominavalen/ui/tcompleto/TcompletoFragment.java
@@ -10,7 +10,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -26,39 +29,44 @@ import java.util.Calendar;
 public class TcompletoFragment extends Fragment {
     EditText fechainicio;
 
-    private FragmentTcompletoBinding binding;
+    //private FragmentTcompletoBinding binding;
+
     @SuppressLint("WrongViewCast")
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tcompleto, container, false);
         TcompletoViewModel tcompletoViewModel = new ViewModelProvider(this).get(TcompletoViewModel.class);
-        binding = FragmentTcompletoBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        final TextView textView = binding.textTcompleto;
+        //binding = FragmentTcompletoBinding.inflate(inflater, container, false);
+        //View root = binding.getRoot();
+        final TextView textView = view.findViewById(R.id.textTcompleto);
         tcompletoViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         fechainicio = view.findViewById(R.id.fechaini);
 
-
-
-        return root;
+        ImageView btnfechaini = view.findViewById(R.id.btnfechaini);
+        btnfechaini.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(requireContext(), "Hola mundo", Toast.LENGTH_SHORT).show();
+                DatePickerDialog d = new DatePickerDialog(requireContext(), new DatePickerDialog.OnDateSetListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        fechainicio.setText(year + "/" + month + "/" + dayOfMonth);
+                    }
+                }, 2024, Calendar.JANUARY, 1);
+                d.show();
+            }
+        });
+        return view;
     }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+        //binding = null;
 
 
     }
 
-    public void calendarioini(View view) {
-        DatePickerDialog d = new DatePickerDialog(requireContext(), new DatePickerDialog.OnDateSetListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                fechainicio.setText(year +"/"+ month + "/" + dayOfMonth);
-            }
-        }, 2024, Calendar.JANUARY, 1);
-        d.show();
-    }
 }
